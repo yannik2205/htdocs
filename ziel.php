f7361c3cc15c4a706f9f5a73e1a9604a0e9d8e67
yannik2205 <63048503+yannik2205@users.noreply.github.com>
2023-02-28 22:36:34 +0100

    trsst

--- ziel.php
+++ ziel.php
@@ -2,16 +2,33 @@
     $_SERVER = "localhost";
     $user="root";
     $pass = "";
-    $db = "ObstLaden";
+    $db = "obstladen";
+    $con = mysqli_connect($_SERVER, $user, $pass);
 
-    //$ID = $_POST["ID"];
     $vorn = $_POST["vname"];
     $nach = $_POST["nname"];
-    //$ort = 
-
-
-    $con = mysqli_connect($_SERVER, $user, $pass);
+    $ort = $_POST["Wohnort"];
+    $sorten = $_POST["Sorte"];
+    $mengen = $_POST["Menge"];
+    $id = 1;
     mysqli_select_db($con, $db);
 
-    $sql = "INSERT INTO tabelle_adre VALUES ('$vorn','$nachn')";
+    $sql = "INSERT INTO bestellungen VALUES ('$id', '$vorn','$nach', '$ort', '$sorten', '$mengen')";
+    $id = $id + 1;
+    mysqli_query($con, $sql);
+
+    $sqlSelectAll = "SELECT * FROM bestellungen";
+    $ergebnis = mysqli_query($con, $sqlSelectAll);
+    while ($row = mysqli_fetch_assoc($ergebnis)){
+        echo $row["Vorname"]." ".
+             $row["Nachname"]." ".
+             $row["Ort"]." ".
+             $row["Sorte"]." ".
+             $row["Menge"]."<br>";
+
+
+
+    }
+
+
 ?>
