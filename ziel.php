8e935812fbd736513fce6adb67450bf805b0336d
yannik2205 <63048503+yannik2205@users.noreply.github.com>
2023-03-01 11:38:24 +0100

    Funktionierend in der Schule gemacht

--- ziel.php
+++ ziel.php
@@ -10,13 +10,13 @@
     $ort = $_POST["Wohnort"];
     $sorten = $_POST["Sorte"];
     $mengen = $_POST["Menge"];
-    $id = 1;
+    $id = "";
     mysqli_select_db($con, $db);
 
-    $sql = "INSERT INTO bestellungen VALUES ('$id', '$vorn','$nach', '$ort', '$sorten', '$mengen')";
-    $id = $id + 1;
+    $sql = "INSERT INTO bestellungen (Vorname, Nachname, Ort, Sorte, Menge) VALUES ('$vorn','$nach', '$ort', '$sorten', '$mengen')";
+    
     mysqli_query($con, $sql);
-
+    echo mysqli_error($con);
     $sqlSelectAll = "SELECT * FROM bestellungen";
     $ergebnis = mysqli_query($con, $sqlSelectAll);
     while ($row = mysqli_fetch_assoc($ergebnis)){
